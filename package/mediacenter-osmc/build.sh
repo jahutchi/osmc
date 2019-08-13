8cddb432bc423d36b0ae9e2d12f3b9b0efc05efe
James Hutchinson <jahutchinson99@googlemail.com>
2019-08-13 08:40:12 +0100

    bumped kodi to 5330b74

--- package/mediacenter-osmc/build.sh
+++ package/mediacenter-osmc/build.sh
@@ -6,7 +6,7 @@
 . ../common.sh
 if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ] || [ "$1" == "pc" ] || [ "$1" == "vero2" ] || [ "$1" == "vero3" ]
 then
-pull_source "https://github.com/xbmc/xbmc/archive/89472b7d690922366bbff5ba47d077f82a9f0ec1.tar.gz" "$(pwd)/src"
+pull_source "https://github.com/xbmc/xbmc/archive/5330b7495b69ef909db5309ab7cf69dad00e9798.tar.gz" "$(pwd)/src"
 API_VERSION="18"
 else
 pull_source "https://github.com/xbmc/xbmc/archive/master.tar.gz" "$(pwd)/kodi"
